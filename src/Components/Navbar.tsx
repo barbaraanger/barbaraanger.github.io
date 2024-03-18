4d70a254f028ad7ebc3ba10e7683438a83ff044b
barbaraanger <barbaraanger@hotmail.com>
2024-03-18 09:15:17 -0300

    merging

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const Navbar: React.FC<any> = () => {
     return (<></>)
 }
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./Components/Header";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
